df91eaabe60be59262a5d89aceff47b4ffcd2db0
Dane Natoli <dane.natoli@gmail.com>
2013-03-14 14:37:25 -0700

    Empty rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DaneNatoliCup2013::Application.config.secret_token = 'e20d7da3900ba5d9d55b4f929b33941a5009c188befe660a73b264e011d1e4d7a0dd178399ea5ea9339ed8b012ab4a9378df1e84bfed05f5f91d7adc86813e31'
