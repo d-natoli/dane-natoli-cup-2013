509e7d4446736b5c63a7d91575b4890032093d19
Dane Natoli <dane.natoli@gmail.com>
2013-08-01 14:37:44 -0700

    Fix up paperclip defaults.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,8 +1,11 @@
-Rails.application.config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+
+if Rails.env.production?
+  Paperclip::Attachment.default_options[:storage] = :s3
+  Paperclip::Attachment.default_options[:s3_credentials] = {
     :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
-}
+end
